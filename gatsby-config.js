71ad14db658650266053d8cb6b47ec536f8e6911
Andrea Valla <valla.andrea@gmail.com>
2021-03-01 15:21:41 +0100

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
