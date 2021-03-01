443a93da06ff7221f197ecfc8e68d30da763e11e
Andrea Valla <valla.andrea@gmail.com>
2021-03-01 15:18:43 +0100

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  siteMetadata: {
+    title: `Andrea Valla`,
+    description: `Andrea Valla - Full Stack Developer`,
+    author: `@avalla`,
+    contacts: {
+      phone: "+39 335 82 30 421",
+      email: "valla.andrea@gmail.com",
+      website: "https://avalla.github.io",
+    },
+    social: {
+      github: "https://github.com/avalla",
+      linkedin: "https://linkedin.com/in/avalla",
+      stackoverflow: "https://stackoverflow.com/users/876314",
+    },
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `avalla-resume`,
+        short_name: `avalla-resume`,
+        start_url: `/`,
+        background_color: `#2979ff`,
+        theme_color: `#2979ff`,
+        display: `minimal-ui`,
+        icon: `src/images/portfolio.svg`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Montserrat`],
+        display: "swap",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-MKCWNDK",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
+  ],
+}
