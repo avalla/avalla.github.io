e2ca9550a24d9d55fe50e7adac95db226f91e6f3
Andrea Valla <valla.andrea@gmail.com>
2023-11-10 15:18:09 +0100

    feat: update resume

--- src/components/resume-components/professional-summary.js
+++ src/components/resume-components/professional-summary.js
@@ -9,7 +9,26 @@ export default function ProfessionalSummary() {
           Experienced Full Stack Developer dedicated to crafting enterprise-level web applications with precision.
           Proficient in both client and server-side programming, ensuring comprehensive solutions from meticulous
           analysis to flawless implementation.
+        </p>import React from 'react';
+
+        export default function ProfessionalSummary() {
+        return (
+        <section className='section'>
+        <h2 className='title is-2'>Professional summary</h2>
+        <article>
+        <p>
+        Experienced Full Stack Developer dedicated to crafting enterprise-level web applications with precision.
+        Proficient in both client and server-side programming, ensuring comprehensive solutions from meticulous
+        analysis to flawless implementation.
         </p>
+        <p> Expert in software engineering with a focus on robust design, coding, testing, and seamless implementation.Passionate about staying ahead with cutting-edge technology, regularly embracing new systems, libraries, and
+        frameworks. Seasoned Linux user with a deep affinity for shells and a passion for mastering the command line.
+        </p>
+        </article>
+        </section>
+        );
+      }
+
         <p> Expert in software engineering with a focus on robust design, coding, testing, and seamless implementation.
         </p>
         <p> Passionate about staying ahead with cutting-edge technology, regularly embracing new systems, libraries, and
