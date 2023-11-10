667c7b85c5128ebf2dc176dec2ff7760a411c7af
Andrea Valla <valla.andrea@gmail.com>
2023-11-10 15:25:17 +0100

    feat: update resume

--- src/components/resume-components/professional-summary.js
+++ src/components/resume-components/professional-summary.js
@@ -7,34 +7,11 @@ export default function ProfessionalSummary() {
       <article>
         <p>
           Experienced Full Stack Developer dedicated to crafting enterprise-level web applications with precision.
-          Proficient in both client and server-side programming, ensuring comprehensive solutions from meticulous
-          analysis to flawless implementation.
-        </p>import React from 'react';
-
-        export default function ProfessionalSummary() {
-        return (
-        <section className='section'>
-        <h2 className='title is-2'>Professional summary</h2>
-        <article>
-        <p>
-        Experienced Full Stack Developer dedicated to crafting enterprise-level web applications with precision.
-        Proficient in both client and server-side programming, ensuring comprehensive solutions from meticulous
-        analysis to flawless implementation.
-        </p>
-        <p> Expert in software engineering with a focus on robust design, coding, testing, and seamless implementation.Passionate about staying ahead with cutting-edge technology, regularly embracing new systems, libraries, and
-        frameworks. Seasoned Linux user with a deep affinity for shells and a passion for mastering the command line.
-        </p>
-        </article>
-        </section>
-        );
-      }
-
-        <p> Expert in software engineering with a focus on robust design, coding, testing, and seamless implementation.
+          Proficient in both client and server-side programming, expert in software engineering with a focus on robust
+          design, coding, testing, and seamless implementation.
         </p>
         <p> Passionate about staying ahead with cutting-edge technology, regularly embracing new systems, libraries, and
-          frameworks.
-        </p>
-        <p> Seasoned Linux user with a deep affinity for shells and a passion for mastering the command line.
+          frameworks. Seasoned Linux user with a deep affinity for shells and a passion for mastering the command line.
         </p>
       </article>
     </section>
